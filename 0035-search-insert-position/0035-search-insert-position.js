438b47577a373faff21b9d54df91e63df80be045
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2026-09-23 13:48:34 +0530

    Time: 0 ms (100%), Space: 53.5 MB (71.57%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    let mid;
+    while(low <= high){
+        mid = Math.floor((low + high) / 2)
+        if(nums[mid] === target){
+            return mid
+        }
+        else if(nums[mid] > target){
+            high = mid - 1
+        }
+        else{
+            low = mid + 1
+        }
+    }
+    return low
+};
