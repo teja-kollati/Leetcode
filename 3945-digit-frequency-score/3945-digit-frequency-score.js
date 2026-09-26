8e8963962a59692a593b18a744d5c5e2538b4249
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2026-09-26 12:58:20 +0530

    Time: 1 ms (75.52%), Space: 55.6 MB (76.57%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var digitFrequencyScore = function(n) {
+    let sum = 0
+    while(n != 0){
+        sum += n % 10
+        n = Math.floor(n / 10)
+    }
+    return sum
+};
